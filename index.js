5eed280c85a00c9851ac45d264a07d309691f101
Francisco Lobo <francisco@lobosalas.com>
2020-03-28 07:39:26 -0600

    Mejoras en comportamiento y separacion de mensajes en archivos de texto para mayor limpieza, cambio de paradigma de comandos a lenguaje mas natural

--- index.js
+++ index.js
@@ -1,68 +1,128 @@
+////////////////////////////////////////////////// 
+// ZINC Discord Bot *Bot CoVida* 27 y 28 de Marzo 2020
+// Escrito por: @marcopaaz del team CoVida 
+// Codigo Adicional @kikolobo del team CoVida
+// Covida Discord: https://discord.gg/PZ99WkY
+////////////////////////////////////////////////// 
 let Discord = require('discord.js')
 let Client = new Discord.Client()
 let dotenv = require('dotenv');
+var fs = require('fs');
 
-// Grabs the BOT_TOKEN from .env and stores in on the `process.env`
+var welcomeMessage = 'no asignado. reportar a moderador';
+var roleInstructions = 'no asignado. reportar a moderador';
+var roleListRaw = 'no asignado. reportar a moderador';
+
+
+// Carga de archivos auxiliares
+fs.readFile('welcomeMessage.txt', 'utf8', function(err, data) {
+  if (err) throw err;
+  welcomeMessage = data;
+});
+
+fs.readFile('roleInstructions.txt', 'utf8', function(err, data) {
+  if (err) throw err;
+  roleInstructions = data;
+});
+
+fs.readFile('roleList.txt', 'utf8', function(err, data) {
+  if (err) throw err;
+  roleListRaw = data;
+});
+
+// Carga de parametros de configuración
 dotenv.load()
 
-let allowedRoles = process.env.ALLOWED_ROLES.split(',')
-let botToken = process.env.BOT_TOKEN
+// Carga de parametros de configuración
+let botname = process.env.BOT_NAME;
+let allowedRoles = process.env.ALLOWED_ROLES.split(',');
+let botToken = process.env.BOT_TOKEN;
+let dalayForRoleMessage = process.env.DELAY_FOR_ROLE_MSG;
+let versionString = process.env.VERSION;
 
-// allowed strings
-let allowedString = ''
-allowedRoles.forEach((role) => {
-  allowedString = allowedString.concat('- ' + role + '\n')
-})
 
+// Carga los roles permitidos en una variable legible
+// let allowedString = ''
+// allowedRoles.forEach((role) => {
+//   allowedString = allowedString.concat('  :radio_button:  ' + role + '\n')
+// })
+
+
+// Comenzamos a escuchr eventos
+// ******************************
+
+// Evento: Cuando llega un nuevo mensaje
 Client.on('message', msg => {
-  // Set prefix
-  let prefix = "!"
+  let args = msg.content.split(" ");  
 
-  if (!msg.content.startsWith(prefix)
-    || msg.author.bot
-  ) return
+  // No es para el bot o es el bot.. descartar.
+  if (!msg.content.toLowerCase().startsWith(botname) || msg.author.bot) {     
+    return;
+  }
 
-  if (msg.content.startsWith(prefix + 'role')) {
 
-    // Tomar args
-    let args = msg.content.split(" ");
+  // Comandos permitidos
+  // ************************** >>
+  if (args[1] == 'bienvenido' || args[1] == 'bienvenida' || args[1] == 'welcome' || args[1] == 'reglas' || args[1] == 'rules') {
+    msg.reply(welcomeMessage);    
+    setTimeout(function(){ //Esperar 1 minuto y mandarle instrucciónes para el rol
+    msg.reply(roleInstructions);
+  }, dalayForRoleMessage);
+    return      
+  }    
 
-    if (args.length < 2 || args[1] == '--help') {
-      msg.channel.sendMessage('Estos son los roles disponibles: \n'+
-        allowedString +
-        '\nusa !role `<rol>` para unirte.')
+  if (args[1] == 'roles' || args[1] == 'rol' || args[1] == 'ayuda' || args[1] == 'help' || args[1] == '?') {
+    msg.reply(roleListRaw);
+    return
+  }
 
-      return
-    }
+  if (args[1] == 'hola' || args[1] == 'hello' || args[1] == 'eit' || args[1] == 'pst' || args[1] == 'ehlo')  {
+    msg.reply('**¡Hola!**, Soy **zinc** :robot:  (v' + versionString + '), el bot de **CoVida!** para servirte!')
+    return;
+  }
 
-    // Asignar Rol
-    let role = msg.guild.roles.find("name", args[1].toLowerCase());
+  if (args[1] == 'version' || args[1] == 'ver') {
+    msg.reply('version: ' + versionString);
+    return
+  }
+
+  if (args[1] == 'soy') {
+    var theRole = args[2].toLowerCase()
+    var role = msg.guild.roles.find(role => role.name === theRole)
+
 
     if (!role || role === null) {
-      msg.channel.sendMessage('No se pudo encontrar el rol, intenta de nuevo.')
+      msg.reply('Ese rol no existe.\n Revisa los roles disponibles escribiendo `zinc roles` o `zinc ayuda`')
       return
     }
 
     if (allowedRoles.indexOf(role.name) === -1) {
-      msg.channel.sendMessage('No tienes permitido unirte a este grupo. \nPara obtener una lista de roles usa: `!role --help`')
+      msg.reply('Ese rol no esta disponible.\n Para obtener una lista de roles usa: `zinc roles`')
       return
     }
 
-    msg.member.addRole(role).catch(console.error);
-    msg.channel.sendMessage('Has sido agregado a: ' + role.name)
+    msg.member.addRole(role).catch(console.error);    
+    msg.channel.send(`:fire: ${msg.member.user.username} es un experto en ${role.name}! **woot! woot!**`)
 
     return
   }
+  // ************************** << Comandos Permitidos
+
+  // Mensaje default cuando no encuentra un comando.
+  msg.reply('Perdón. No entendí lo que me quieres decir, escribe *`zinc ayuda`* para mas info');
 })
 
+
+// Evento: Cuando llega un usuario nuevo al server.
 Client.on("guildMemberAdd", member => {
-    console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-
-    member.guild.defaultChannel.sendMessage(`Bienvenido "${member.user.username}"! \n Asegúrate de escoger tu rolc con "!role" y el nombre del rol \n\nReglas: \n0.- Por favor NO SPAM - NO FLOODS \n1.- Busca el canal más adecuado para postear. \n2.- No postear avances de otros proyectos mas que en el canal de #noticias-de-otros-proyectos \n3.- Si tienes una idea para un proyecto la discutimos en #posibles-proyectos \n4.- Para todo lo demás… Esta #todo-lo-demas :)` );
+  //Mandarle mensaje de bienvenida.
+  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );  
+  member.send(welcomeMessage);  
 })
 
+// Evento: Cuando corre el servidor
 Client.on('ready', () => {
-  Client.user.setGame('Usa !role --help')
+  Client.user.setActivity('Pidele ayuda a zinc nuestro bot -> zinc ayuda')
   console.log(`Ready to set roles in ${Client.channels.size} channels on ${Client.guilds.size} servers, for a total of ${Client.users.size} users.`)
 })
 
